2854d84c3248ac237a1cba72c34cc73847013733
Tyler Cipriani <thcipriani@gmail.com>
2011-03-03 19:17:40 -0800

    Devise User and Link model communicating

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redditalready::Application.config.secret_token = 'fe9ef7bdfac143cedcf39f85a38f677d187ce620a2fe68811eb69757d7289dbce7fcc9257ca958e18f93380ecc41ed065cf4efcb242af909dba806ad37904ce4'
